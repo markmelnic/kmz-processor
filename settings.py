6e6f79109cdef7108fa51197920a2d2340847590
Mark Melnic <markhappy200@gmail.com>
2020-11-26 11:43:44 +0100

    Added dataframe columns setting

--- settings.py
+++ settings.py
@@ -2,3 +2,4 @@ ZIP_KML_DOC = "doc.kml"
 CSV_KML_DOC = "kml_index.csv"
 ZIP_KMZ_IMG_FOLDER = "files"
 KMZ_GLOBAL_IMAGE = "map.jpg"
+DF_COLUMNS = ["index", "image", "draw_order", "north", "south", "east", "west", "rotation"]
